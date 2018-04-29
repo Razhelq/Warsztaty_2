2eee368d9611bed02bd4adda8caeb66aa417c2d1
Mateusz Szpakowski <mateuszszpakowski@wp.pl>
2018-04-29 14:35:39 +0200

    starter

---
+++ connect.py
@@ -0,0 +1,24 @@
+from psycopg2 import connect
+import json
+
+app = Flask(__name__)
+
+config_path = "db.json"
+
+def connection(config_file):
+    cfg_string = open(config_file).read()
+    cfg_json = json.loads(cfg_string)
+    return connect(user=cfg_json["username"],
+                   password=cfg_json["passwd"],
+                   host=cfg_json["hostname"],
+                   database=cfg_json["db_name"])
+
+try:
+    cnx = connection(config_path)
+    print("Połączono z bazą")
+except Exception as e:
+    print("Niepowodzenie połączenia:", e)
+    exit()
+
+
+cursor = cnx.cursor()
